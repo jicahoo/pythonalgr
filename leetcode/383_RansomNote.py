cc9fd55742fed3bc62bfe7d4e1849e2958e585e1
Jichao <jicahoo@163.com>
2016-08-29 11:32:41 -0400

    Done 383. Leetcode didn't get enough submission. So no score for now.

---
+++ leetcode/383_RansomNote.py
@@ -0,0 +1,66 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        ret = True
+        sorted_randomNote = sorted(ransomNote)
+        sorted_magazine = sorted(magazine)
+        n = len(sorted_randomNote)
+        m = len(sorted_magazine)
+        last_find_start_point = 0
+        for i in xrange(n):
+            if i > m-1:
+                ret = False
+                break
+            else:
+                letter_in_ransom = sorted_randomNote[i]
+                idx = self.find(sorted_magazine, last_find_start_point, letter_in_ransom)
+                if idx is None:
+                    ret = False
+                    break
+                else:
+                    last_find_start_point = idx+1
+        return ret
+
+    def find(self, my_list, start, element):
+        n = len(my_list)
+        for i in xrange(start, n):
+            e = my_list[i]
+            if element == e:
+                return i
+        return None
+
+if __name__ == '__main__':
+    s = Solution()
+    ransom = "a"
+    mag = "b"
+    assert not s.canConstruct(ransom, mag)
+
+    ransom = "a"
+    mag = "ab"
+    assert s.canConstruct(ransom, mag)
+
+    ransom = "aaa"
+    mag = "abaa"
+    assert s.canConstruct(ransom, mag)
+
+    ransom = "a"
+    mag = ""
+    assert not s.canConstruct(ransom, mag)
+
+    ransom = "zx"
+    mag = "aaaxaeeezee"
+    assert s.canConstruct(ransom, mag)
+
+    ransom = "aa"
+    mag = "ab"
+    assert not s.canConstruct(ransom, mag)
+
+    ransom = "fihjjjjei"
+    mag =  "hjibagacbhadfaefdjaeaebgi"
+    print sorted(ransom)
+    print sorted(mag)
+    assert not s.canConstruct(ransom, mag)
